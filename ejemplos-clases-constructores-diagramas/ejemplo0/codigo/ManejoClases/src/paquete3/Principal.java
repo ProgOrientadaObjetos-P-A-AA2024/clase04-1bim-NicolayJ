224fedd4e0dd17779030cd65188b5b6725bccacb
NicolayJ <dnjimenez6@utpl.edu.ec>
2024-05-01 16:33:28 -0500

    Cambios Clase 2

---
+++ ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete3/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete3;
+
+public class Principal {
+    public static void main(String[] args) {
+        /* En esta clase principal en el metodo main se ha creado un objeto de la
+        clase computadora que toma el CONSTRUCTOR por DEFECTO de la clase 
+        computadora ya que en dicha clase no hay ningun constructor definido y por
+        lo tanto toma el constructor por defecto 
+        */
+        
+        Computadora computadoraTrabajo = new Computadora();
+    }
+}
